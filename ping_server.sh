2ac991a924e335de9390b048b37d7ecd17111de7
Trevor S Wetzel <wetzel11@purdue.edu>
2026-09-07 16:43:54 -0400

    First attempt at AWK for splitting results

--- ping_server.sh
+++ ping_server.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Responsible for pinging a single server and determining its stats
+#
+# Usage:
+# ping_server.sh [ip_addr]
 
-NPINGS=4
+NPINGS=2
 
-ping -c $NPINGS $1
+ping -c $NPINGS $1 > ping_results.txt
+
+cat ping_results.txt | grep rtt | awk -F "=" '{print $2}'
