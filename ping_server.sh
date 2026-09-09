5812548e65a3e2d6d9a8dcd6073582ad6a1613cf
Trevor S Wetzel <wetzel11@purdue.edu>
2026-09-09 01:50:15 -0400

    Output min, avg, and max to stdout for a server

--- ping_server.sh
+++ ping_server.sh
@@ -5,8 +5,12 @@
 # Usage:
 # ping_server.sh [ip_addr]
 
-NPINGS=2
+NPINGS=4
 
 ping -c $NPINGS $1 > ping_results.txt
 
-cat ping_results.txt | grep rtt | awk -F "=" '{print $2}'
+line=$(cat ping_results.txt | grep rtt | awk -F "=" '{print $2}')
+
+echo $line | awk -F "/" '{print $1}'
+echo $line | awk -F "/" '{print $2}'
+echo $line | awk -F "/" '{print $3}'
